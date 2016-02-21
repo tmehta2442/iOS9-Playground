33d82ee3de43782319277924260ae1da42c14dbb
tmehta2442 <tmehta@post.harvard.edu>
2016-02-21 11:24:02 -0800

    basics

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("hello")
+print("world")
+
+
+print(str)
+
+class Dog {
+    func bark() {
+        print("woof")
+    }
+}
+
+let dog = Dog()
+dog.bark()
