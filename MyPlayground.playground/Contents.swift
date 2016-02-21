4d2b280de8c8c5cc677e343eac5870df426003f9
tmehta2442 <tmehta@post.harvard.edu>
2016-02-21 11:44:07 -0800

    some epic coding lolz

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -9,6 +9,7 @@ print("world")
 
 
 print(str)
+/////////
 
 class Dog {
     func bark() {
@@ -18,3 +19,28 @@ class Dog {
 
 let dog = Dog()
 dog.bark()
+/////////
+
+var one = 1
+var two = 2
+
+two = one
+
+print(two)
+/////////////
+//let is a constant
+
+let three = 3
+var four = 4
+
+//the below will fail
+//three = four
+
+
+//////
+
+//type stays constant and established when variable declared and can never change
+var five = 2
+//below will fail
+//five = "hello"
+
